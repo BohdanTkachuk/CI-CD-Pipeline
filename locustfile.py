19b606430638237af151f73fa6a2766e378343d1
bohdan <tkachuk_bohdan@ukr.net>
2020-11-19 11:03:42 +0100

    add locust, upgrade Makefile

---
+++ locustfile.py
@@ -0,0 +1,16 @@
+import time
+from locust import HttpUser, task
+
+class QuickstartUser(HttpUser):
+    @task
+    def home_page(self):
+        self.client.get("/")
+   
+
+    @task
+    def on_start(self):
+        self.client.post("/predict", json={"CHAS":{"0":0}, "RM":{"0":6.575}, "TAX":{"0":296.0}, "PTRATIO":{"0":15.3}, "B":{"0":396.9}, "LSTAT":{"0":4.98}})
+
+
+
+
